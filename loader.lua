92734f35aadfddee33fb03da2b271a70d991af65
Zegalexy <112091320+zegalexy@users.noreply.github.com>
2023-01-18 19:36:25 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,8 @@
 local gamelist = {
     [994732206]  = "https://raw.githubusercontent.com/zegalexy/UniqueV2/main/KaitanV2",
-    [2644656496] = "https://raw.githubusercontent.com/zegalexy/UniqueV2/main/ProjectNewWorld"
+    [2644656496] = "https://raw.githubusercontent.com/zegalexy/UniqueV2/main/ProjectNewWorld",
+    [994732206]  = "https://raw.githubusercontent.com/REALSALFES/BloxFruit/main/BLoxFruit",
+    [3183403065]  = "https://raw.githubusercontent.com/zegalexy/UniqueV2/main/AnimeAdventure"
 }
 local checkgame = gamelist[game.gameId]
 if gamelist[game.gameId] then 
